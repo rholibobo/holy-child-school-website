81d3bac59f63454a6d1fe40f17118eda4c3a86d5
Otuekong Idongesit <fizy500@gmail.com>
2023-09-07 22:00:46 +0100

    Fixed Teachers data

--- src/app/components/Carousels/Carousels.jsx
+++ src/app/components/Carousels/Carousels.jsx
@@ -16,11 +16,24 @@ import pix2 from "../../../../public/images/HomePage/port5.jpg";
 import pix3 from "../../../../public/images/HomePage/port6.jpg";
 
 // Teachers carousel imports
+import Teacher1 from "../../../../public/images/Teachers/1d-2-transparentbg.png";
+import Teacher2 from "../../../../public/images/Teachers/1a.jpeg";
+import Teacher3 from "../../../../public/images/Teachers/1d.jpeg";
+import Teacher4 from "../../../../public/images/Teachers/1f.jpeg";
+import Teacher5 from "../../../../public/images/Teachers/1qs.jpeg";
+import Teacher6 from "../../../../public/images/Teachers/1s.jpeg";
+import Teacher7 from "../../../../public/images/Teachers/123.jpeg";
+import Teacher8 from "../../../../public/images/Teachers/1234.jpeg";
+import Teacher9 from "../../../../public/images/Teachers/12345.jpeg";
+import Teacher10 from "../../../../public/images/Teachers/123456.jpeg";
+import Teacher11 from "../../../../public/images/Teachers/wed.jpeg";
+
+//
 
 import { Blob } from "../TeachersComponents/TeachersComponents";
 import { FcNext } from "react-icons/fc";
 import { FcPrevious } from "react-icons/fc";
-import { teachersData } from "@/app/teachers/teachersData";
+// import { teachersData } from "@/app/teachers/teachersData";
 
 // import { CarouselRightButton, CarouselLeftButton } from "../Carousels/";
 import { CarouselLeftButton, CarouselRightButton } from "../Buttons/AllButtons";
@@ -33,13 +46,13 @@ export function MediaCard() {
       arrows
       autoPlaySpeed={3000}
       centerMode={false}
-      className=""
-      containerClass="container"
-      dotListClass=""
+      className=''
+      containerClass='container'
+      dotListClass=''
       draggable
       focusOnSelect={false}
       infinite={false}
-      itemClass=""
+      itemClass=''
       keyBoardControl
       minimumTouchDrag={80}
       pauseOnHover
@@ -77,7 +90,7 @@ export function MediaCard() {
       rtl={false}
       shouldResetAutoplay
       showDots={false}
-      sliderClass=""
+      sliderClass=''
       slidesToSlide={1}
       swipeable
     >
@@ -90,18 +103,18 @@ export function MediaCard() {
       >
         <CardMedia
           sx={{ height: "55vh" }}
-          image="/images/HomePage/prenursery-1.jpg"
-          title="girl child"
+          image='/images/HomePage/prenursery-1.jpg'
+          title='girl child'
         />
 
         <CardContent sx={{ backgroundColor: "#1faded" }}>
           <Typography
             gutterBottom
-            variant="h6"
-            textAlign="center"
-            component="div"
-            fontWeight="bold"
-            color="#ffffff"
+            variant='h6'
+            textAlign='center'
+            component='div'
+            fontWeight='bold'
+            color='#ffffff'
           >
             Pre-Nursery
           </Typography>
@@ -116,18 +129,18 @@ export function MediaCard() {
       >
         <CardMedia
           sx={{ height: "55vh" }}
-          image="/images/HomePage/nursery.jpg"
-          title="girl child"
+          image='/images/HomePage/nursery.jpg'
+          title='girl child'
         />
 
         <CardContent sx={{ backgroundColor: "#bb56e2" }}>
           <Typography
             gutterBottom
-            variant="h6"
-            textAlign="center"
-            component="div"
-            fontWeight="bold"
-            color="#ffffff"
+            variant='h6'
+            textAlign='center'
+            component='div'
+            fontWeight='bold'
+            color='#ffffff'
           >
             Nursery
           </Typography>
@@ -142,18 +155,18 @@ export function MediaCard() {
       >
         <CardMedia
           sx={{ height: "55vh" }}
-          image="/images/HomePage/primary.jpg"
-          title="girl child"
+          image='/images/HomePage/primary.jpg'
+          title='girl child'
         />
 
         <CardContent sx={{ backgroundColor: "#919497" }}>
           <Typography
             gutterBottom
-            variant="h6"
-            textAlign="center"
-            component="div"
-            fontWeight="bold"
-            color="#ffffff"
+            variant='h6'
+            textAlign='center'
+            component='div'
+            fontWeight='bold'
+            color='#ffffff'
           >
             Primary
           </Typography>
@@ -182,109 +195,109 @@ export function MediaCard() {
 // Homepage Hero Section Carousel
 export function HeroSectionCarousel() {
   return (
-  <Carousel
-    additionalTransfrom={0}
-    arrows
-    autoPlay
-    autoPlaySpeed={4000}
-    centerMode={false}
-    className=""
-    containerClass="container-with-dots"
-    dotListClass=""
-    draggable
-    focusOnSelect={false}
-    infinite={false}
-    itemClass=""
-    keyBoardControl
-    minimumTouchDrag={80}
-    pauseOnHover
-    renderArrowsWhenDisabled={false}
-    renderButtonGroupOutside={false}
-    renderDotsOutside={false}
-    responsive={{
-      desktop: {
-        breakpoint: {
-          max: 3000,
-          min: 1024,
+    <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlay
+      autoPlaySpeed={4000}
+      centerMode={false}
+      className=''
+      containerClass='container-with-dots'
+      dotListClass=''
+      draggable
+      focusOnSelect={false}
+      infinite={false}
+      itemClass=''
+      keyBoardControl
+      minimumTouchDrag={80}
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
+          },
+          items: 1,
+          partialVisibilityGutter: 40,
         },
-        items: 1,
-        partialVisibilityGutter: 40,
-      },
-      mobile: {
-        breakpoint: {
-          max: 464,
-          min: 0,
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
+          },
+          items: 1,
+          partialVisibilityGutter: 30,
         },
-        items: 1,
-        partialVisibilityGutter: 30,
-      },
-      tablet: {
-        breakpoint: {
-          max: 1024,
-          min: 464,
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
+          },
+          items: 1,
+          partialVisibilityGutter: 30,
         },
-        items: 1,
-        partialVisibilityGutter: 30,
-      },
-    }}
-    rewind
-    rewindWithAnimation={false}
-    rtl={false}
-    shouldResetAutoplay
-    showDots={false}
-    sliderClass=""
-    slidesToSlide={2}
-    swipeable
-  >
-    <Box >
-      <Image
-        src={pix1}
-        alt="Carousel Image"
-        style={{
-          width: "100%",
-          height: "auto",
-          borderRadius: "20% 10% 30% 20%",
-          border: "2px solid #ffffff",
-        }}
-      />
-    </Box>
-    <Box >
-      <Image
-        src={pix2}
-        alt="Carousel Image"
-        style={{
-          width: "100%",
-          height: "auto",
-          borderRadius: "20% 10% 30% 20%",
-          border: "2px solid #ffffff",
-        }}
-      />
-    </Box>
-    <Box >
-      <Image
-        src={pix2}
-        alt="Carousel Image"
-        style={{
-          width: "100%",
-          height: "auto",
-          borderRadius: "20% 10% 30% 20%",
-          border: "2px solid #ffffff",
-        }}
-      />
-    </Box>
-    <Box>
-      <Image
-        src={pix3}
-        alt="Carousel Image"
-        style={{
-          width: "100%",
-          height: "auto",
-          borderRadius: "20% 10% 30% 20%",
-          border: "2px solid #ffffff",
-        }}
-      />
-    </Box>
-  </Carousel>
+      }}
+      rewind
+      rewindWithAnimation={false}
+      rtl={false}
+      shouldResetAutoplay
+      showDots={false}
+      sliderClass=''
+      slidesToSlide={2}
+      swipeable
+    >
+      <Box>
+        <Image
+          src={pix1}
+          alt='Carousel Image'
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: "20% 10% 30% 20%",
+            border: "2px solid #ffffff",
+          }}
+        />
+      </Box>
+      <Box>
+        <Image
+          src={pix2}
+          alt='Carousel Image'
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: "20% 10% 30% 20%",
+            border: "2px solid #ffffff",
+          }}
+        />
+      </Box>
+      <Box>
+        <Image
+          src={pix2}
+          alt='Carousel Image'
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: "20% 10% 30% 20%",
+            border: "2px solid #ffffff",
+          }}
+        />
+      </Box>
+      <Box>
+        <Image
+          src={pix3}
+          alt='Carousel Image'
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: "20% 10% 30% 20%",
+            border: "2px solid #ffffff",
+          }}
+        />
+      </Box>
+    </Carousel>
   );
 }
 
@@ -299,38 +312,116 @@ const ButtonGroup = ({ next, previous, ...rest }) => {
       <button
         className={`${
           currentSlide === 0 ? "disable" : ""
-        } iconsDiv bg-pink-500 active:bg-gray-400 transition  duration-100 absolute right-full `}
+        } iconsDiv bg-[#1c1464] active:bg-gray-400 transition  duration-100 absolute right-full `}
         onClick={() => previous()}
       >
-        <FcPrevious className="text-gray-200" />
+        <FcPrevious className='text-gray-200' />
       </button>
 
       {/* Next button */}
 
       <button
-        className="iconsDiv bg-pink-500  active:bg-gray-400 transition  duration-100 absolute left-full "
+        className='iconsDiv bg-[#1c1464]  active:bg-gray-400 transition  duration-100 absolute left-full '
         onClick={() => next()}
       >
-        <FcNext className="text-gray-200" />
+        <FcNext className='text-gray-200' />
       </button>
     </div>
   );
 };
 
 export function TeachersCarousel() {
+  const TeachersData = [
+    {
+      id: 1,
+      image: Teacher1,
+      name: "John Henry",
+      area_of_discipline: "Home economics",
+    },
+    {
+      id: 2,
+      image: Teacher2,
+      name: "Peter parker",
+      area_of_discipline: "Commerce",
+    },
+    {
+      id: 3,
+      image: Teacher3,
+      name: "David Living-Stone",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 4,
+      image: Teacher4,
+      name: "David Francis",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 5,
+      image: Teacher5,
+      name: "Peter Andrew",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 6,
+      image: Teacher6,
+      name: "Peter Andrew",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 7,
+      image: Teacher7,
+      name: "Holy Cow",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 8,
+      image: Teacher8,
+      name: "Peter Andrew",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 9,
+      image: Teacher9,
+      name: "Peter Andrew",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 10,
+      image: Teacher10,
+      name: "Holy Shit",
+      area_of_discipline: "Agricultural Science",
+    },
+    {
+      id: 11,
+      image: Teacher11,
+      name: "Peter Andrew",
+      area_of_discipline: "Agricultural Science",
+    },
+  ];
+
+  const content = TeachersData.map((teacher) => (
+    <Blob
+      key={teacher.id}
+      image={teacher.image}
+      name={teacher.name}
+      area_of_discipline={teacher.area_of_discipline}
+    />
+  ));
+
   return (
-    <div className="relative">
+    <div className='relative'>
       <Carousel
         additionalTransfrom={0}
         autoPlaySpeed={3000}
         centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
+        className=''
+        containerClass='container'
+        dotListClass=''
         draggable
         focusOnSelect={false}
         infinite={true}
-        itemClass=""
+        itemClass=''
         keyBoardControl
         minimumTouchDrag={80}
         pauseOnHover
@@ -352,7 +443,7 @@ export function TeachersCarousel() {
               max: 464,
               min: 0,
             },
-            items: 2,
+            items: 1,
             partialVisibilityGutter: 30,
           },
           tablet: {
@@ -364,25 +455,16 @@ export function TeachersCarousel() {
             partialVisibilityGutter: 30,
           },
         }}
-        rewind={false}
-        rewindWithAnimation={false}
+        rewind={true}
+        rewindWithAnimation={true}
         rtl={false}
         shouldResetAutoplay
         showDots={false}
-        sliderClass=""
+        sliderClass=''
         slidesToSlide={1}
         swipeable
       >
-        {/* {teachersData.map((teacher) => {
-          <Blob key={teacher.id} {...teacher} />;
-        })} */}
-
-        <Blob />
-        <Blob />
-        <Blob />
-        <Blob />
-        <Blob />
-        <Blob />
+        {content}
       </Carousel>
     </div>
   );
--- src/app/teachers/page.jsx
+++ src/app/teachers/page.jsx
@@ -40,10 +40,20 @@ export default function Teachers() {
       </section>
 
       <section className='py-20'>
-        <h2 className='text-gray-900 font-bold  text-2xl max-width'>
+        <h2 className='text-gray-900 font-bold  text-2xl text-center space-y-4 pb-8'>
           Who we are
         </h2>
-        <div className='max-width grid grid-rows-1 lg:grid-cols-2 lg:gap-20 '>
+        <div className='max-width grid grid-rows-1 lg:grid-cols-2 lg:gap-20 relative'>
+          <Image
+            src='/images/DesignImages/crown.png '
+            className='w-fit absolute  left-1'
+            width={100}
+            height={100}
+            aria-hidden='true'
+            alt='red star'
+          />
+
+          {/* Here */}
           <div className='mask1 w-80 h-80 '>
             <Image
               src='/images/Teacher-Training/DSC0407-1024x684.jpg'
@@ -57,10 +67,10 @@ export default function Teachers() {
           <div className='grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-6'>
             <div className='space-y-4 bg-gray-100  rounded-sm p-3 '>
               <div className='flex items-center space-x-2  '>
-                <div className='iconsDiv bg-purple-400 border border-purple-400 flex justify-center items-end w-fit'>
+                <div className='iconsDiv bg-[#1c1464] border-purple-400 flex justify-center items-end w-fit'>
                   <FcGraduationCap className='text-3xl' />
                 </div>
-                <p className=' t text-base font-bold'>
+                <p className='text-base font-bold'>
                   Self-contained gifted programs
                 </p>
               </div>
@@ -70,7 +80,7 @@ export default function Teachers() {
             </div>
             <div className='space-y-4 bg-gray-100  rounded-sm p-3 '>
               <div className='flex items-center space-x-2 '>
-                <div className='iconsDiv bg-green-400 border border-green-400 flex justify-center items-end w-fit pb-7'>
+                <div className='iconsDiv bg-[#1c1464] border border-green-400 flex justify-center items-end w-fit pb-7'>
                   <FcGraduationCap className='text-3xl' />
                 </div>
                 <p className=' text-base font-bold'>
@@ -83,7 +93,7 @@ export default function Teachers() {
             </div>
             <div className='space-y-4 bg-gray-100  rounded-sm p-3 '>
               <div className='flex items-center space-x-2 '>
-                <div className='iconsDiv bg-lime-600 border border-lime-600 flex justify-center items-end w-fit pb-7'>
+                <div className='iconsDiv bg-[#1c1464] border border-lime-600 flex justify-center items-end w-fit pb-7'>
                   <FcGraduationCap className='text-3xl' />
                 </div>
                 <p className='text-base font-bold'>
@@ -96,7 +106,7 @@ export default function Teachers() {
             </div>
             <div className='spac space-y-4 bg-gray-100  rounded-sm p-3'>
               <div className='flex items-center space-x-2 '>
-                <div className='iconsDiv bg bg-sky-600 border--400 flex justify-center items-end w-fit pb-7'>
+                <div className='iconsDiv bg bg-[#1c1464] flex justify-center items-end w-fit pb-7'>
                   <FcGraduationCap className='text-3xl' />
                 </div>
                 <p className=' text-base font-bold'>
@@ -110,7 +120,7 @@ export default function Teachers() {
           </div>
         </div>
       </section>
-      <section className="grid grid-cols-1 justify-center items-center my-10 after:content-[''] ">
+      <section className="grid grid-cols-1 justify-center items-center my-10 ">
         <div className=''>
           <h2 className='text-center text-gray-900 font-bold  text-2xl'>
             Our Team
@@ -129,19 +139,19 @@ export default function Teachers() {
         </div>
       </section>
 
-      <section className='bg-[#1c1464] before:contents-[url(../../public/images/DesignImages/cropped_div.png)] my-14'>
+      <section className='bg-[#1c1464]  mt-14'>
         <div className='max-width grid grid-rows-1 lg:grid-cols-2 items-center justify-between lg:gap-20 '>
-          <div className='grid grid-cols-2 justify-center items-center gap-10'>
+          <div className='grid grid-cols-2 justify-center items-center py-5 gap-10'>
             <div className='flex items-center space-x-2 '>
-              <div className='iconsDiv bg-lime-600 border border-lime-600 flex justify-center items-end w-fit pb-7'>
-                <FcGraduationCap className='text-3xl' />
+              <div className='iconsDiv bg-[#FFA500] border border-[#FFA500] flex justify-center items-end w-fit pb-7'>
+                <FcGraduationCap className='text-3xl  text-white' />
               </div>
               <p className=' text-gray-200 text-base font-bold'>
                 Self-contained gifted programs
               </p>
             </div>
             <div className='flex items-center space-x-2 '>
-              <div className='iconsDiv bg-lime-600 border border-lime-600 flex justify-center items-end w-fit pb-7'>
+              <div className='iconsDiv bg-[#FFA500] border border-[#FFA500] flex justify-center items-end w-fit pb-7'>
                 <FcGraduationCap className='text-3xl' />
               </div>
               <p className='w-1/2 text-gray-200 text-base font-bold'>
@@ -149,7 +159,7 @@ export default function Teachers() {
               </p>
             </div>
             <div className='flex items-center space-x-2 '>
-              <div className='iconsDiv  bg-yellow-400 border border-red-600 flex justify-center items-end w-fit pb-7'>
+              <div className='iconsDiv  bg-[#FFA500] border border-[#FFA500] flex justify-center items-end w-fit pb-7'>
                 <FcGraduationCap className='text-3xl' />
               </div>
               <p className=' text-gray-200 text-base font-bold'>
@@ -157,7 +167,7 @@ export default function Teachers() {
               </p>
             </div>
             <div className='flex items-center space-x-2 '>
-              <div className='iconsDiv bg-sky-600 border border-blue-600 justify-center items-end w-fit pb-7'>
+              <div className='iconsDiv bg-[#FFA500] border border-[#FFA500] justify-center items-end w-fit pb-7'>
                 <FcGraduationCap className='text-3xl' />
               </div>
               <p className=' text-gray-200 text-base font-bold'>
@@ -178,13 +188,18 @@ export default function Teachers() {
           </div>
         </div>
       </section>
-      {/* <section>
-        <h2 className='text-center font-bold text-lg'>Curriculum</h2>
-      
-        <div>
-          <div></div>
-        </div>
-      </section> */}
+      <svg
+        data-name='Layer 1'
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 1200 120'
+        // viewBox="0 100 1200 500"
+        preserveAspectRatio='none'
+      >
+        <path
+          d='M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z'
+          className='fill-[#1c1464]'
+        ></path>
+      </svg>
     </main>
   );
 }
