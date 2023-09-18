e6b418a3db4848cfda49ff88a1e8d9c20c8f3a4a
Otuekong Idongesit <fizy500@gmail.com>
2023-09-18 20:26:00 +0100

    Fixed Teachers page

--- src/app/components/TeachersComponents/TeachersComponents.jsx
+++ src/app/components/TeachersComponents/TeachersComponents.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 export function Blob({ image, name, area_of_discipline }) {
@@ -18,6 +21,90 @@ export function Blob({ image, name, area_of_discipline }) {
       <p className=' text-gray-500 font-medium text-base'>
         {area_of_discipline}
       </p>
+    </div>
+  );
+}
+
+// Quote starts here
+
+const quotes = [
+  {
+    id: 1,
+    quote: "Education is not preparation for life; education is life itself.",
+    author: "John Dewey",
+  },
+  {
+    id: 2,
+    quote:
+      "What sculpture is to a block of marble, education is to a human soul.",
+    author: "Joseph Addison",
+  },
+
+  {
+    id: 3,
+    quote:
+      "Education breeds confidence. Confidence breeds hope. Hope breeds peace.",
+    author: "Confucius",
+  },
+  {
+    id: 4,
+    quote:
+      "Better than a thousand days of diligent study is one day with a great teacher.",
+    author: "Japanese Proverb",
+  },
+  {
+    id: 5,
+    quote:
+      "A good teacher is like a candle—it consumes itself to light the way for others.",
+    author: "Mustafa Kemal Atatürk",
+  },
+  {
+    id: 6,
+    quote: "Children must be taught how to think, not what to think.",
+    author: "Margaret Mead",
+  },
+  {
+    id: 7,
+    quote:
+      "What we want is to see the child in pursuit of knowledge, and not knowledge in pursuit of the child.",
+    author: "George Bernard Shaw",
+  },
+  {
+    id: 8,
+    quote: "It takes a big heart to help shape little minds.",
+    author: "Unknown",
+  },
+  {
+    id: 9,
+    quote:
+      "Education is the most powerful weapon which you can use to change the world.",
+    author: "Nelson Mandela",
+  },
+  {
+    id: 10,
+    quote:
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    author: "Benjamin Franklin",
+  },
+];
+export function Quote() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      setActiveIndex((prev) => (prev + 1 >= quotes.length ? 0 : prev + 1));
+    }, 10000);
+
+    return () => {
+      window.clearInterval(timer);
+    };
+  });
+
+  const activequote = quotes[activeIndex];
+  return (
+    <div className='py-10 px-4 border-l-3 border-l-blue-500 space-y-4'>
+      <p className='text-white text-xl font-semibold'>{activequote.quote}</p>
+      <p className='text-gray-200 font-light'>{activequote.author}</p>
     </div>
   );
 }
