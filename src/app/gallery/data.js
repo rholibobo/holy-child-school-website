cc757ebe6d0d98204b0eff7ada2235ce68250944
Nathaniyell <essien.nathan@yahoo.com>
2023-09-21 00:02:49 +0100

    update gallery component

---
+++ src/app/gallery/data.js
@@ -0,0 +1,25 @@
+import image1 from "../../../public/images/Gallery/img1.jpg";
+import image2 from "../../../public/images/Gallery/img17.jpg";
+import image3 from "../../../public/images/Gallery/img6.jpg";
+import image4 from "../../../public/images/Gallery/img15.jpg";
+import image5 from "../../../public/images/Gallery/img14.jpg";
+import image6 from "../../../public/images/Gallery/img13.jpg";
+import image7 from "../../../public/images/Gallery/img12.jpg";
+import image8 from "../../../public/images/Gallery/img10.jpg";
+import image9 from "../../../public/images/Gallery/img9.jpg";
+import image10 from "../../../public/images/Gallery/img11.jpg";
+
+const imageUrl = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+];
+
+export default imageUrl
--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -6,16 +6,36 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { FcExpand, FcCollapse } from "react-icons/fc";
 import Header from "../components/Header/Header";
+import imageUrl from "./data";
 
 export default function Gallery() {
-  
-
   return (
     <main>
       <div className={styles.navbar}>
-          <Header />
+        <Header />
       </div>
-      <div className={styles.section1}></div>
+      <div className={styles.section1}>
+        <div className="flex justify-center items-center p-5 h-full flex-col text-center text-white w-full">
+          <h1 className="font-bold uppercase text-[50px] mb-2">
+            Gallery Section
+          </h1>
+          <p className="w-full md:w-1/2">
+            Welcome to our school&apos;s Gallery section, where we proudly
+            showcase the vibrant and creative spirit that defines our
+            educational community. Our gallery serves as a window into the rich
+            tapestry of experiences, achievements, and talents that make our
+            school special. Here, you will find a captivating collection of
+            images that capture the essence of our school life.
+          </p>
+        </div>
+      </div>
+      <section>
+        {imageUrl.map((image, index) => {
+          <div className="grid grid-cols-2 gap-5">
+            <Image key={index} src={image} alt={image + index} />
+          </div>;
+        })}
+      </section>
     </main>
   );
 }
