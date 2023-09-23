ab373bdfc1199495074c23419b1a0593f51f0e53
Nathaniyell <essien.nathan@yahoo.com>
2023-09-23 13:55:09 +0100

    add all images and update component for better readbility

--- src/app/gallery/Nav.jsx
+++ src/app/gallery/Nav.jsx
@@ -1,12 +1,25 @@
-"use client"
+"use client";
+import { motion } from "framer-motion";
 
-const Nav = ({onclick}) => {
+const Nav = ({ onclick }) => {
   return (
-    <nav className="flex w-11/12 md:ml-4 md:w-1/4 justify-between items-center border-b-2 border-blue-300 p-2 my-3">
-        <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[16px] px-4 py-2.5 text-center" onClick={()=>onclick("student")}>Students</button>
-        <button className="bg-blue-700 text-white hover:text-blue-700 border border-blue-700 hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[16px] px-4 py-2.5 text-center" onClick={()=>onclick("staff")}>Teachers</button>
+    <nav className="flex w-11/12 md:ml-4 md:w-1/4 justify-between items-center p-2 mt-3">
+      <motion.button
+        whileTap="tap"
+        className="bg-blue-700 text-white hover:text-blue-700 border border-blue-700 hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[16px] px-4 py-2.5 text-center"
+        onClick={() => onclick("student")}
+      >
+        Students
+      </motion.button>
+      <motion.button
+        whileTap="tap"
+        className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[16px] px-4 py-2.5 text-center active:bg-blue-700"
+        onClick={() => onclick("staff")}
+      >
+        Teachers
+      </motion.button>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/app/gallery/data.js
+++ src/app/gallery/data.js
@@ -8,6 +8,8 @@ import image7 from "../../../public/images/Gallery/img12.jpg";
 import image8 from "../../../public/images/Gallery/img10.jpg";
 import image9 from "../../../public/images/Gallery/img9.jpg";
 import image10 from "../../../public/images/Gallery/img11.jpg";
+import image11 from "../../../public/images/Gallery/contact.jpg";
+import image12 from "../../../public/images/Gallery/service2.jpg";
 
 import staff1 from '../../../public/images/Teacher-Training/teacher1.jpg'
 import staff2 from '../../../public/images/Teacher-Training/teacher2.jpg'
@@ -19,26 +21,47 @@ import staff7 from '../../../public/images/Teacher-Training/teacher7.jpg'
 import staff8 from '../../../public/images/Teacher-Training/teacher8.jpg'
 import staff9 from '../../../public/images/Teacher-Training/teacher9.jpg'
 import staff10 from '../../../public/images/Teacher-Training/teacher10.jpg'
-const imageUrl = [
-  image1,
-  image2,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-];
-export const staff = [
-staff1,
-staff2,
-staff3,
-staff5,
-staff6,
-staff7,
-staff8,
-staff9,
-staff10
-]
-export default imageUrl
+import staff11 from '../../../public/images/Teacher-Training/teacher11.jpg'
+import staff12 from '../../../public/images/Teacher-Training/teacher12.jpg'
+import staff13 from '../../../public/images/Teacher-Training/teacher13.jpg'
+import staff14 from '../../../public/images/Teacher-Training/teacher14.jpg'
+import staff15 from '../../../public/images/Teacher-Training/teacher15.jpg'
+import staff16 from '../../../public/images/Teacher-Training/teacher16.jpg'
+
+
+
+const data = {
+  student:  [
+    image1,
+    image11,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image12
+  ],
+  staff: [
+    staff1,
+    staff2,
+    staff3,
+    staff4,
+    staff5,
+    staff6,
+    staff7,
+    staff8,
+    staff9,
+    staff10,
+    staff11,
+    staff12,
+    staff13,
+    staff14,
+    staff15,
+    staff16,
+    ]
+}
+export default data
