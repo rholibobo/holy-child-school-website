eadcbd55cb2ef9d6aaa057ebe05f1470e47a1a15
Nathaniyell <essien.nathan@yahoo.com>
2023-09-25 09:59:30 +0100

    add function to only show modal when image is clicked

--- src/app/gallery/Modal.jsx
+++ src/app/gallery/Modal.jsx
@@ -11,15 +11,17 @@ const Modal = (props) => {
   };
   return (
     <>
-      <div className="dismiss" onClick={handleClick}>
-        {clickedImg && <Image src={clickedImg} alt="Bigger Image" />}
-        <span className="dismiss" onClick={handleClick}>
-          X
-        </span>
-        <span onClick={handleCickRightNavigation}>
-          <FaArrowRight />
-        </span>
-      </div>
+      {clickedImg && (
+        <div className="dismiss" onClick={handleClick}>
+          <Image src={clickedImg} alt="Bigger Image" />
+          <span className="dismiss" onClick={handleClick}>
+            X
+          </span>
+          <span onClick={handleCickRightNavigation}>
+            <FaArrowRight />
+          </span>
+        </div>
+      )}
     </>
   );
 };
